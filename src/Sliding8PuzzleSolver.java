fa5c1e742e039f37fd770123edf2d8fd763d43ca
Kevin Risden <compuwizard123@gmail.com>
2010-01-25 16:05:51 -0500

    Basic layout of code

--- src/Sliding8PuzzleSolver.java
+++ src/Sliding8PuzzleSolver.java
@@ -1,4 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 
 public class Sliding8PuzzleSolver {
+	
+	public static void main(String args[]) {
+	//  prompt the user to enter their name
+	      System.out.print("Enter start state: ");
+
+	      //  open up standard input
+	      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	      String startState = null;
+
+	      //  read the username from the command-line; need to use try/catch with the
+	      //  readLine() method
+	      try {
+	         startState = br.readLine();
+	      } catch (IOException ioe) {
+	         System.out.println("IO error trying to read your name!");
+	         System.exit(1);
+	      }
+	      long startStateInt = new Long(startState);
+	      if( startStateInt < 12345678 || startStateInt > 876543210) {
+	    	  throw new IllegalArgumentException();
+	      }
+	      
+	      System.out.println(startState);
+	      
+	      /*
+	       * 
+	       * startState -> makeBabies
+	       * loop on priorityQueue
+	       * makeBabies on each
+	       * check goal
+	       * 
+	       */
+	}
+	public class state {
+		private int heuristicValue;
+		
+		public state() {
+			
+		}
+		
+		// TODO return anything?
+		private void makeBabies(long state) {
+			/*
+			 * 
+			 * makes arrayList of babies
+			 * goes through arraylist
+			 * discardCopies
+			 * evaluate on remaining
+			 * put into priority queue
+			 * 
+			 */
+			
+		}
+		
+		private void discardCopies(long state, ArrayList visited) {
+			
+		}
+		
+		private void evaluate(long state, long goalState) {
+			
+		}
+	}
 
 }
