b4ebabcc24b3bd6d09a9fa2098ab79bf914b50b6
Kevin Risden <compuwizard123@gmail.com>
2010-01-27 13:41:15 -0500

    Added junit test file

---
+++ src/Testing.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+
+
+public class Testing extends TestCase {
+	
+	public void testingMakeBabies(){
+		
+	}
+	
+	public static void main(String args[]) {
+		junit.swingui.TestRunner.run(Testing.class);
+	}
+}
